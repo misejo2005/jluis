c434d4ac62a08a1a14c78912b51a07f643e3ef30
misejo2005 <60907004+misejo2005@users.noreply.github.com>
2020-02-13 21:00:15 -0600

    Bigotes o impresion de controlador a vista

--- src/app/utilidades/encabezado/encabezado.component.ts
+++ src/app/utilidades/encabezado/encabezado.component.ts
@@ -7,11 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EncabezadoComponent implements OnInit {
 
-  
+  public titulo : string = "TITULO CON MAYUSCULA";
+  public codigo : number = 34774
+
+  public clase : String = "fondo";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public getStatus() : string {
+   return "Nombre";
+  }
+
+  public getTotal(importe : number) {
+    return importe * 1.16;
   }
 
 }
